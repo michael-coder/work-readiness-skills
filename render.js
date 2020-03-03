76dfee2654da889bc4a204b0bf99d98cf18ff51f
Yuanyuan LI <yuanyuan.li@amadeus.com>
2020-03-03 13:38:58 +0800

    add more skills also enable GA

--- render.js
+++ render.js
@@ -2,11 +2,28 @@ $( document ).ready(function() {
   // Handler for .ready() called.
 var Obj = {
 	"skills":[
-	{"name":"HTML", "progress":90, "class": "progress-bar progress-bar-warning"},
-	{"name":"CSS", "progress":80, "class": "progress-bar"},
-	{"name":"JavaScript", "progress":70, "class": "progress-bar"},
-	{"name":"Docker", "progress":10, "class": "progress-bar progress-bar-danger"},
-	{"name":"K8S", "progress":10, "class": "progress-bar progress-bar-danger"},
+	{"name":"Agile", "progress":70, "class": "progress-bar progress-bar-info"},
+	{"name":"SAFe Agile", "progress":70, "class": "progress-bar progress-bar-info"},
+	{"name":"PMP", "progress":1, "class": "progress-bar progress-bar-danger"},
+	
+	{"name":"HTML", "progress":90, "class": "progress-bar progress-bar-success"},
+	{"name":"CSS", "progress":90, "class": "progress-bar progress-bar-success"},
+	{"name":"JavaScript", "progress":90, "class": "progress-bar progress-bar-success"},
+	{"name":"JQuery", "progress":70, "class": "progress-bar progress-bar-danger"},
+	{"name":"Java", "progress":50, "class": "progress-bar progress-bar-warning"},
+	{"name":"Perl", "progress":70, "class": "progress-bar progress-bar-warning"},
+	{"name":"MySQL", "progress":60, "class": "progress-bar progress-bar-warning"},
+	{"name":"Git", "progress":50, "class": "progress-bar progress-bar-warning"},
+	{"name":"JSON", "progress":50, "class": "progress-bar progress-bar-warning"},
+	{"name":"XML", "progress":50, "class": "progress-bar progress-bar-warning"},
+	{"name":"YAML", "progress":10, "class": "progress-bar progress-bar-danger"},
+	{"name":"Windows", "progress":50, "class": "progress-bar progress-bar-warning"},
+	{"name":"Linux", "progress":70, "class": "progress-bar progress-bar-warning"},
+	{"name":"Bash", "progress":60, "class": "progress-bar progress-bar-warning"},
+	
+	{"name":"Cloud Computing", "progress":20, "class": "progress-bar progress-bar-danger"},
+	{"name":"Docker", "progress":20, "class": "progress-bar progress-bar-danger"},
+	{"name":"Kubernetes ", "progress":20, "class": "progress-bar progress-bar-danger"},
 ]  
 };
 
@@ -20,6 +37,33 @@ for (i in Obj.skills) {
     </div>
   </div>`;
 }
+
+var sampleHtml = `
+  <p>The contextual classes colors the progress bars:</p> 
+  <div class="progress">
+    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+      40% Complete (success)
+    </div>
+  </div>
+  <div class="progress">
+    <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+      50% Complete (info)
+    </div>
+  </div>
+  <div class="progress">
+    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+      60% Complete (warning)
+    </div>
+  </div>
+  <div class="progress">
+    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+      70% Complete (danger)
+    </div>
+  </div>
+`;
+
+//html += sampleHtml;
+
 console.log(html);
 console.log($(".container"));
 $(".container").append(html);  
